85a883f1318afc01b0be0ce9ea12ea1195bcb8ad
czke33 <czke33@gmail.com>
2024-01-18 12:16:46 +0200

    try something to add gallery

--- parcel-project/src/js/01-gallery.js
+++ parcel-project/src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from '.gallery-items.js';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    item =>
+      `<li class="gallery_item">
+      <a class="gallery__link" href=${item.original}>
+      <img class="gallery__image"
+      src=${item.preview} 
+      alt=${item.description} /></a></li>`
+  )
+  .join(' ');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
