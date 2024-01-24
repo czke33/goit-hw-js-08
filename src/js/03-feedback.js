060711fb204f6c927363998728e28bffcb8c2d9f
czke33 <czke33@gmail.com>
2024-01-24 13:24:07 +0200

    remade the 3 form hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,46 @@
 import _throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
 const formEl = document.querySelector('.feedback-form');
-const LOCAL_STORAGE = 'feedback-form-state';
+const emailEl = formEl[(name = 'email')];
+const textEl = formEl[(name = 'message')];
 
-let data = {};
+savedStorageValue();
 
-loadForm();
+formEl.addEventListener('submit', onSubmit);
+formEl.addEventListener('input', _throttle(onAddText, 500));
 
-formEl.addEventListener('input', _throttle(onSaveFormInput, 500));
-
-formEl.addEventListener('submit', onFormSubmit);
-
-function onSaveFormInput(event) {
-  data = JSON.parse(localStorage.getItem(LOCAL_STORAGE)) || {};
-
-  data[event.target.name] = event.target.value;
-
-  localStorage.setItem(LOCAL_STORAGE, JSON.stringify(data));
-}
-
-function onFormSubmit(event) {
-  event.preventDefault();
-  if (!event.target.email.value || !event.target.message.value) {
-    alert('Enter all data');
+function onSubmit(evt) {
+  evt.preventDefault();
+  if (emailEl.value === '' || textEl.value === '') {
     return;
   }
-
-  event.target.reset();
-  console.log(data);
-  localStorage.removeItem(LOCAL_STORAGE);
+  const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(savedText);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
-function loadForm() {
-  try {
-    let formLoad = JSON.parse(localStorage.getItem(LOCAL_STORAGE));
-    if (!formLoad) {
-      return;
-    }
+function onAddText(evt) {
+  const allData = {
+    email: emailEl.value,
+    message: textEl.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(allData));
+  allData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(allData));
+}
 
-    data = formLoad;
-    formEl.email.value = data.email || '';
-    formEl.message.value = data.message || '';
-  } catch (error) {
-    console.error('Error_message ', error.message);
+function savedStorageValue() {
+  const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedText) {
+    if (savedText.message) {
+      textEl.value = savedText.message;
+    }
+    if (savedText.email) {
+      emailEl.value = savedText.email;
+    }
   }
 }
